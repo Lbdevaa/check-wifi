73f8b07a2d81f7cae6b99be5ca3c848df25d1e19
Lebedev Andrey <lbdev.aa@gmail.com>
2023-05-18 20:42:59 +0500

    first

---
+++ js/app.js
@@ -0,0 +1,39 @@
+window.addEventListener('load', async () => {
+  if ('serviceWorker' in navigator) {
+    try {
+      const reg = await navigator.serviceWorker.register('/sw.js');
+      console.log('Service worker register success', reg);
+    } catch (e) {
+      console.log('Service worker register fail');
+    }
+  }
+
+  await loadPosts();
+});
+
+async function loadPosts() {
+  const res = await fetch(
+    'https://jsonplaceholder.typicode.com/posts?_limit=11'
+  );
+  const data = await res.json();
+
+  const container = document.querySelector('#posts');
+  if (navigator.connection) {
+    const { effectiveType } = navigator.connection;
+    container.textContent = `Тип соединения: ${effectiveType}`;
+  }
+  container.innerHTML = ``;
+}
+
+function toCard(post) {
+  return `
+    <div class="card">
+      <div class="card-title">
+        ${post.title}
+      </div>
+      <div class="card-body">
+        ${post.body}
+      </div>
+    </div>
+  `;
+}
