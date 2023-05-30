aea166f2a696fec0c1ecd08311f371c4aa489891
Lebedev Andrey <lbdev.aa@gmail.com>
2023-05-30 15:28:46 +0500

    fix: icons

--- js/app.js
+++ js/app.js
@@ -1,14 +1,12 @@
 window.addEventListener('load', async () => {
   if ('serviceWorker' in navigator) {
     try {
-      const reg = await navigator.serviceWorker.register('/check-wifi//sw.js');
+      const reg = await navigator.serviceWorker.register('/check-wifi/sw.js');
       console.log('Service worker register success', reg);
     } catch (e) {
       console.log('Service worker register fail');
     }
   }
-
-  await loadPosts();
 });
 
 window.addEventListener('load', () => {
@@ -27,45 +25,3 @@ window.addEventListener('load', () => {
     }
   }
 });
-
-if (navigator.connection) {
-  const { effectiveType } = navigator.connection;
-  if (effectiveType === 'wifi') {
-    console.log('Пользователь подключен к Wi-Fi');
-  } else if (effectiveType === 'cellular') {
-    console.log('Пользователь подключен к мобильному интернету');
-  }
-}
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', async () => {
-    try {
-      const reg = await navigator.serviceWorker.register('/check-wifi//sw.js');
-      console.log('Service worker registered!', reg);
-    } catch (err) {
-      console.log('Service worker registration failed:', err);
-    }
-  });
-}
-
-async function loadPosts() {
-  const res = await fetch(
-    'https://jsonplaceholder.typicode.com/posts?_limit=11'
-  );
-  const data = await res.json();
-
-  // container.innerHTML = ``;
-}
-
-function toCard(post) {
-  return `
-    <div class="card">
-      <div class="card-title">
-        ${post.title}
-      </div>
-      <div class="card-body">
-        ${post.body}
-      </div>
-    </div>
-  `;
-}
